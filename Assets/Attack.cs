1f1be003783109e77bc105c34e04fb29394bf057
Paul Sinnett <paul.sinnett@gmail.com>
2020-03-17 13:05:56 +0000

    Initial commit

---
+++ Assets/Attack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetInteger("Attack", 1);
+        }
+    }
+    public void AttackFinished()
+    {
+        animator.SetInteger("Attack", 0);
+    }
+}
